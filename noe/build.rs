aa561316f63d79d718b97aa6157d07ec28131bdb
Nathaniel McCallum <npmccallum@redhat.com>
2020-01-27 16:14:09 +0000

    Remove unnecessary link args

--- noe/build.rs
+++ noe/build.rs
@@ -5,8 +5,6 @@ fn main() {
         .file("src/start.s")
         .compile("start");
 
-    println!("cargo:rustc-bin-link-arg={}", "-Wl,--no-gc-sections");
-    println!("cargo:rustc-bin-link-arg={}", "-nodefaultlibs");
     println!("cargo:rustc-bin-link-arg={}", "-nostdlib");
     println!("cargo:rustc-bin-link-arg={}", "-static");
 }
