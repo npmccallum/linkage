0b0c9a1cd4357d95a9651e52c1fbc024fb42a50d
Nathaniel McCallum <npmccallum@redhat.com>
2020-01-27 16:11:24 +0000

    Fix indent in start.s

--- noe/src/start.s
+++ noe/src/start.s
@@ -1,6 +1,6 @@
-	.text
-	.globl _start
-	.type _start, @function
+    .text
+    .globl _start
+    .type _start, @function
 
 _start:
     call       get_msg
@@ -9,11 +9,11 @@ _start:
     call       get_len
     mov %rax,  %r14
 
-	mov $1,    %rax /* SYS_write */
-	mov $1,    %rdi /* STDOUT_FILENO */
-	mov %r15,  %rsi /* string */
-	mov %r14,  %rdx /* length */
-	syscall
+    mov $1,    %rax /* SYS_write */
+    mov $1,    %rdi /* STDOUT_FILENO */
+    mov %r15,  %rsi /* string */
+    mov %r14,  %rdx /* length */
+    syscall
 
     mov $60,   %rax /* SYS_exit */
     mov $0,    %rdi /* exit status */
